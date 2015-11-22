4fd09f7e8f721a25f77032c7e8abad07e79a380b
Jason Aquino <gitlabjason@mindtester.com>
2015-11-22 09:56:03 -0500

    Updated Project class

--- Types/Project.cs
+++ Types/Project.cs
@@ -2,21 +2,47 @@
 using Newtonsoft.Json;
 using unirest_net.http;
 using System.Web;
-
-
+using System.Collections.Generic;
 
 namespace GitLab
 {
     public partial class GitLab
     {
+        /// <summary>
+        /// GitLab Project Class
+        /// </summary>
+        /// <remarks>
+        /// See https://gitlab.com/help/api/projects.md for reference
+        /// </remarks>
         public class Project
         {
-            //See https://gitlab.com/help/api/projects.md for reference
-
-            public string name, path, description, import_url, ssh_url_to_repo, http_url_to_repo, web_url;
-            public int namespace_id, visibility_level;
-            public bool issues_enabled, merge_requests_enabled, wiki_enabled, snippets_enabled;
-
+            public string name
+                , name_with_namespace
+                , path
+                , path_with_namespace
+                , description
+                , default_branch
+                , import_url
+                , ssh_url_to_repo
+                , http_url_to_repo
+                , web_url
+                , created_at
+                , last_activity_at
+                , avatar_url;
+            public string[] tag_list;
+            public int id, namespace_id, visibility_level, creator_id;
+            public bool issues_enabled, merge_requests_enabled, wiki_enabled, snippets_enabled , builds_enabled, archived;
+            public Owner owner;
+   
+            /// <summary>
+            /// Creates a new project
+            /// </summary>
+            /// <param name="_Config"></param>
+            /// <param name="_Name">The name of the new project</param>
+            /// <param name="_Description"> Optional Description, can be null</param>
+            /// <param name="_Namespace">Namespace to create the project in</param>
+            /// <param name="_VisibilityLevel">New project's visibility level</param>
+            /// <returns></returns>
             public static Project Create(Config _Config, string _Name, string _Description, Namespace _Namespace, VisibilityLevel _VisibilityLevel)
             {
                 Project RetVal = JsonConvert.DeserializeObject<Project>(Unirest.post(_Config.APIUrl + "projects?name=" + HttpUtility.UrlEncode(_Name)
@@ -30,11 +56,33 @@ namespace GitLab
                 return RetVal;
             }
 
+            /// <summary>
+            /// List all projects
+            /// </summary>
+            /// <param name="_Config"></param>
+            /// <returns></returns>
+            public static List<Project> List(Config _Config)
+            {                               
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            /// Project Visibility Level as enum for ease of use;
+            /// </summary>
             public enum VisibilityLevel
             {
                 Private = 0
                 , Internal = 10
                 , Public = 20
+            }
+
+            /// <summary>
+            /// Project owner sub-class so that owner can be parsed from JSON
+            /// </summary>
+            public class Owner
+            {
+                public int id;
+                public string name, created_at;
             }
         }
     }
