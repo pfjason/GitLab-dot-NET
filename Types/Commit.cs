758c5337e96bcb98021f7cbf7bc2fca0297c1943
Jason Aquino <gitlabjason@mindtester.com>
2015-11-22 11:43:16 -0500

    Update Commit class

--- Types/Commit.cs
+++ Types/Commit.cs
@@ -13,9 +13,22 @@ namespace GitLab
     {
         public partial class Project
         {
+            /// <summary>
+            /// Gitlab Project Commit object
+            /// </summary>
             public class Commit
             {
-                public string id, short_id, title, author_name, author_email, created_at, message;
+                public string id
+                    , short_id
+                    , title
+                    , author_name
+                    , author_email
+                    , created_at
+                    , message
+                    , committer_name
+                    , committer_email
+                    , authored_date
+                    , committed_date;
                 public string[] parent_ids;
                 public bool allow_failure;
             }
